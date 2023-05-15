206337d2ec8c525b09977a6e1887f8d7f45adc17
yeahyeah <113980774+mariaalonsan@users.noreply.github.com>
2023-05-15 18:37:16 +0200

    ejers

---
+++ pregunta3.py
@@ -0,0 +1,54 @@
+import networkx as nx
+
+# Apartado a
+heroes = ['Iron Man', 'The Incredible Hulk', 'Khan', 'Thor', 'Captain America', 'Ant-Man', 'Nick Fury', 'The Winter Soldier']
+
+twitter_matrix = [
+    [0, 75, 40, 16, 80, 20, 99, 23],
+    [75, 0, 50, 67, 79, 38, 99, 41],
+    [40, 50, 0, 17, 75, 52, 85, 28],
+    [16, 67, 17, 0, 11, 50, 90, 36],
+    [80, 79, 75, 11, 0, 26, 12, 56],
+    [20, 38, 52, 50, 26, 0, 55, 61],
+    [99, 99, 85, 90, 12, 55, 0, 10],
+    [23, 41, 28, 36, 56, 61, 10, 0]
+]
+
+instagram_matrix = [
+    [0, 61, 44, 66, 56, 74, 11, 65],
+    [12, 0, 47, 41, 12, 38, 99, 41],
+    [41, 23, 0, 45, 12, 89, 42, 14],
+    [12, 69, 11, 0, 12, 50, 78, 63],
+    [89, 19, 72, 11, 0, 26, 12, 56],
+    [72, 34, 21, 65, 12, 0, 78, 41],
+    [12, 87, 35, 99, 42, 15, 0, 10],
+    [33, 41, 24, 61, 45, 41, 11, 0]
+]
+
+twitter_graph = nx.Graph()
+instagram_graph = nx.Graph()
+
+# Apartado b
+for i in range(len(heroes)):
+    for j in range(i+1, len(heroes)):
+        twitter_graph.add_edge(heroes[i], heroes[j], weight=twitter_matrix[i][j], label='Twitter')
+        instagram_graph.add_edge(heroes[i], heroes[j], weight=instagram_matrix[i][j], label='Instagram')
+
+# Apartado c
+max_tree_twitter = nx.maximum_spanning_tree(twitter_graph)
+max_tree_instagram = nx.maximum_spanning_tree(instagram_graph)
+
+# Apartado d
+has_path_twitter = nx.has_path(twitter_graph, 'Captain America', 'Nick Fury')
+
+# Apartado e
+has_path_social = nx.has_path(twitter_graph, 'The Winter Soldier', 'Iron Man') or nx.has_path(instagram_graph, 'The Winter Soldier', 'Iron Man')
+
+# Apartado f
+thor_follows = [n for n in instagram_graph.neighbors('Thor')]
+
+print(f"Árbol de expansión máximo para Twitter: {max_tree_twitter.edges(data=True)}")
+print(f"Árbol de expansión máximo para Instagram: {max_tree_instagram.edges(data=True)}")
+print(f"¿Es posible conectar a Captain America con Nick Fury en Twitter?: {'Sí' if has_path_twitter else 'No'}")
+print(f"¿Es posible conectar a The Winter Soldier con Iron Man en alguna red social?: {'Sí' if has_path_social else 'No'}")
+print(f"Thor sigue a las siguientes personas en Instagram: {thor_follows}")
---
+++ pregunta5.py
@@ -0,0 +1,70 @@
+import numpy as np
+from heapq import heappop, heappush
+
+# Nombres de los personajes
+personajes = ['Iron Man', 'The Incredible Hulk', 'Khan', 'Thor', 'Captain America', 'Ant-Man', 'Nick Fury', 'The Winter Soldier']
+
+# Matriz de adyacencia
+grafo = np.array([
+  [0, 6, 0, 1, 8, 7, 3, 2],
+  [6, 0, 0, 6, 1, 8, 9, 1],
+  [0, 0, 0, 1, 2, 1, 5, 0],
+  [1, 6, 1, 0, 1, 5, 9, 3],
+  [8, 1, 2, 1, 0, 2, 4, 5],
+  [7, 8, 1, 5, 2, 0, 1, 6],
+  [3, 9, 5, 9, 4, 1, 0, 1],
+  [2, 1, 0, 3, 5, 6, 1, 0]
+])
+
+# Algoritmo de Prim
+def prim(grafo, inicio):
+    n = len(grafo)
+    visitados = [False]*n
+    pesos = [-1]*n
+    previos = [None]*n
+    pesos[inicio] = 0
+    cola_prioridad = [(0, inicio)]
+
+    while cola_prioridad:
+        peso, u = heappop(cola_prioridad)
+        if not visitados[u]:
+            visitados[u] = True
+            for v, peso_uv in enumerate(grafo[u]):
+                if not visitados[v] and (pesos[v] == -1 or pesos[v] < peso_uv):
+                    pesos[v] = peso_uv
+                    previos[v] = u
+                    heappush(cola_prioridad, (peso_uv, v))
+    return previos
+
+# Apartado a
+inicio = personajes.index('Iron Man')
+previos = prim(grafo, inicio)
+print("Árbol de expansión máximo desde Iron Man:")
+for i, p in enumerate(previos):
+    if p is not None:
+        print("Apartado a:")
+        print(f'{personajes[p]} -- {personajes[i]}')
+
+# Apartado b
+max_episodios = np.max(grafo)
+print(f'\nMáximo número de episodios compartidos: {max_episodios}')
+print('Pares de personajes que comparten el máximo número de episodios:')
+indices = np.where(grafo == max_episodios)
+for i in range(len(indices[0])):
+    if indices[0][i] < indices[1][i]:
+        print("Apartado b:")
+        print(f'{personajes[indices[0][i]]} -- {personajes[indices[1][i]]}')
+
+# Apartado c
+print('\nTodos los personajes:')
+for personaje in personajes:
+    print("Apartado c:")
+    print(personaje)
+
+# Apartado d
+print('\nPersonajes que aparecieron en nueve episodios de la saga:')
+indices_nueve_episodios = np.where(grafo == 9)
+for i in range(len(indices_nueve_episodios[0])):
+    if indices_nueve_episodios[0][i] < indices_nueve_episodios[1][i]:
+        print("Apartado a:")
+        print(f'{personajes[indices_nueve_episodios[0][i]]} -- {personajes[indices_nueve_episodios[1][i]]}')
